979db0ef7ba47f5bce00c35fe2d4e3475881b481
Lucky2307 <alana.luki@gmail.com>
2023-06-24 04:19:44 +0700

    func: added game path grabbing from log

--- src/extract_link.rs
+++ src/extract_link.rs
@@ -1,16 +1,16 @@
 pub mod extract_link {
-    use std::{collections::HashMap, hash::Hash};
+    use std::{collections::HashMap, io::ErrorKind, io::Error};
     use regex::{Regex};
     #[derive(Debug)]
     pub struct ParsedLink {
         url: String,
         queries: HashMap<String, String>,
     }
-    pub fn from_string(data: &str) -> Result<ParsedLink, &str> {
+    pub fn from_string(data: &str) -> Result<ParsedLink, Error> {
         let link_regex = Regex::new(r"https:\/\/.+?getGachaLog").unwrap();
         let link = match link_regex.captures_iter(&data).into_iter().last() {
-            None => return Err("No link_path found"),
-            Some(found_link) => found_link.get(0).unwrap().as_str().to_string(),
+            None => return Err(Error::new(ErrorKind::NotFound, "Link not found")),
+            Some(found_link) => found_link.get(0).unwrap().as_str().to_owned(),
         };
 
         let queries_regex_raw = vec![
@@ -25,11 +25,11 @@ pub mod extract_link {
         for query_regex_raw in queries_regex_raw {
             let query_regex = Regex::new(query_regex_raw).unwrap();
             let query = match query_regex.captures_iter(&data).into_iter().last(){
-                None => return Err("Required query not found"),
-                Some(found_query) => found_query.get(0).unwrap().as_str().to_string(),
+                None => return Err(Error::new(ErrorKind::NotFound, "Required query not found")),
+                Some(found_query) => found_query.iter().last().unwrap().unwrap().as_str(),
             };
             let query_split: Vec<&str> = query.split("=").collect();
-            queries_hashmap.insert(query_split[0].to_string(), query_split[1].to_string());
+            queries_hashmap.insert(query_split[0].to_owned(), query_split[1].to_owned());
         };
         Ok(ParsedLink {
             url: link,
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,23 @@ mod extract_link;
 
 use std::fs::File;
 use std::io::Read;
+use std::path::Path;
 use std::str::from_utf8_unchecked;
+use directories:: BaseDirs;
+use regex::Regex;
 
-use crate::extract_link::extract_link::from_string;
+use extract_link::extract_link::from_string;
+
 fn main() -> std::io::Result<()> {
-    let mut data = File::open("data_3")?;
+    let playerlog_path = BaseDirs::new().unwrap().home_dir().join(Path::new("AppData\\LocalLow\\Cognosphere\\Star Rail\\Player.log"));
+    let mut player_log = File::open(playerlog_path)?;
+    let mut player_log_buffer = String::new();
+    player_log.read_to_string(&mut player_log_buffer)?;
+    let game_path_regex = Regex::new(r".:/.+(StarRail_Data)").unwrap();
+    let game_path = game_path_regex.captures_iter(&player_log_buffer).into_iter().last().unwrap().get(0).unwrap().as_str().to_owned();
+    println!("{game_path}");
+    
+    let mut data = File::open("data_2")?;
     let mut buffer = vec![];
     data.read_to_end(&mut buffer)?;
 
